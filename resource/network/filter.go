ccf7fddd19c3e1723b923afd85de0ed5522c5500
Lenka <lenocka369@gmail.com>
2019-04-16 13:36:06 +0200

    Add filter for network

---
+++ resource/network/filter.go
@@ -0,0 +1,23 @@
+package network
+
+import (
+	"sync"
+
+	"github.com/goat-project/goat-one/resource"
+)
+
+// Filter to filter network data.
+type Filter struct {
+}
+
+// CreateFilter creates Filter.
+func CreateFilter() *Filter {
+	return &Filter{}
+}
+
+// Filtering - only for VM relevant.
+func (f *Filter) Filtering(res resource.Resource, filtered chan resource.Resource, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	filtered <- res
+}
