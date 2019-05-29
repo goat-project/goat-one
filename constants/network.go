68ca2f8abc6ff3267de86c4a1de2de20d9b03bf9
Lenka <lenocka369@gmail.com>
2019-05-29 12:27:16 +0200

    Add constants for network

--- constants/network.go
+++ constants/network.go
@@ -1,5 +1,14 @@
 package constants
 
+// prefix for network subcommands
+const cfgNetworkPrefix = "network."
+
+// constants for network subcommand
 const (
-// TODO: add constants for network flags here
+	// CfgNetworkSiteName represents string of network site name
+	CfgNetworkSiteName = cfgNetworkPrefix + "site-name"
+	// CfgNetworkCloudType represents string of network cloud type
+	CfgNetworkCloudType = cfgNetworkPrefix + "cloud-type"
+	// CfgNetworkCloudComputeService represents string of network cloud compute service
+	CfgNetworkCloudComputeService = cfgNetworkPrefix + "cloud-compute-service"
 )
