dd62a8c8284a34923a26bc87617b3eaec5643cf7
Lenka <lenocka369@gmail.com>
2019-07-11 13:05:36 +0200

    Add Attribute function to Resource
    To use Attribute function for initialization

--- resource/interface.go
+++ resource/interface.go
@@ -3,6 +3,7 @@ package resource
 // Resource interface represents resource from OpenNebula.
 type Resource interface {
 	ID() (int, error)
+	Attribute(path string) (string, error)
 }
 
 // PageSize is a number of read resources in one call. It is used for resources with pagination.
