17709165c9c15e2a2d5318467f98c670f7affa2c
Lenka <lenocka369@gmail.com>
2019-09-18 09:54:08 +0200

    Move constants with error messages

---
+++ constants/error.go
@@ -0,0 +1,21 @@
+package constants
+
+// constants with error messages
+const (
+	ErrCreatePrepLimiterNil = "error create Preparer when limiter is nil"
+	ErrCreatePrepConnNil    = "error create Preparer when gRPC client connection is nil"
+
+	ErrPrepEmptyNetUser = "error prepare empty NetUser"
+	ErrPrepNoNetUser    = "error get id, unable to prepare network record"
+
+	ErrPrepIPv4 = "unable to prepare ipv4 network record"
+	ErrPrepIPv6 = "unable to prepare ipv6 network record"
+
+	ErrPrepWrite = "error write network record"
+
+	ErrNoSiteName  = "no site name in configuration"
+	ErrNoCloudType = "no cloud type in configuration"
+	ErrNoGroupName = "no group name"
+
+	ErrCreateProcReaderNil = "error create Processor when Reader is nil"
+)
